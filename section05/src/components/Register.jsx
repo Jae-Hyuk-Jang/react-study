95f5bce7f7ab7489f5c3d732f8069bd7090c8ec1
Jae-Hyuk-Jang <lucentejjh@gmail.com>
2026-04-26 20:02:49 +0900

    study: useRef() 개념 및 기본 활용법 학습 [핵심 개념] - 컴포넌트의 생명주기 전체에 걸쳐 유지되는 가변(mutable) 객체를 반환하는 훅. - 반환된 객체의  프로퍼티를 통해 값을 읽거나 수정할 수 있음.
    [useState와의 차이점]
    - 상태(state)와 달리 값이 변경되어도 컴포넌트의 리렌더링(Re-render)을 트리거하지 않음.
    - 불필요한 렌더링을 방지하면서 내부 변수 상태를 동기화하고 유지해야 할 때 적합함.
    
    [주요 활용처]
    1. DOM 요소 직접 제어: 특정 input에 focus 주기, 미디어(audio/video) 요소 직접 제어, 스크롤 위치 측정 등.
    2. 렌더링과 무관한 로컬 변수 저장: 타이머 ID(setTimeout/setInterval) 저장, 이전 렌더링의 상태값 기록 등.

--- section05/src/components/Register.jsx
+++ section05/src/components/Register.jsx
@@ -14,20 +14,31 @@ const Register = () => {
     bio: "",
   });
 
-  const refObj = useRef();
-  console.log(refObj);
+  const countRef = useRef(0);
+  const inputRef = useRef();
 
   const onChange = (e) => {
+    countRef.current++;
+    console.log(countRef.current);
     setInput({
       ...input,
       [e.target.name]: e.target.value,
     });
   };
 
+  const onSubmit = () => {
+    if (input.name === "") {
+      //  이름을 입력하는 dom요소에 포커스
+      //console.log(inputRef.current);
+      inputRef.current.focus();
+    }
+  };
+
   return (
     <div>
       <div>
         <input
+          ref={inputRef}
           name="name"
           value={input.name}
           onChange={onChange}
@@ -35,8 +46,28 @@ const Register = () => {
         />
       </div>
       <div>
-        <input type="date" />
+        <input
+          name="birth"
+          value={input.birth}
+          onChange={onChange}
+          type="date"
+        />
       </div>
+
+      <div>
+        <select name="country" value={input.country} onChange={onChange}>
+          <option value=""></option>
+          <option value="kr">한국</option>
+          <option value="us">미국</option>
+          <option value="uk">영국</option>
+        </select>
+      </div>
+
+      <div>
+        <textarea name="bio" value={input.bio} onChange={onChange} />
+      </div>
+
+      <button onClick={onSubmit}>제출</button>
     </div>
   );
 };
