4835a75acf82159be5627ba805b60715dac3a2bc
Jae-Hyuk-Jang <lucentejjh@gmail.com>
2026-05-10 10:45:03 +0900

    chore: project font, png, css

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -4,6 +4,7 @@ import Diary from "./pages/Diary";
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Notfound from "./pages/Notfound";
+import { getEmotionImage } from "./util/get-emotion-image";
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
@@ -15,6 +16,9 @@ function App() {
   };
   return (
     <>
+      <div>
+        <img src={getEmotionImage(1)} />
+      </div>
       <div>
         <Link to={"/"}>Home</Link>
         <Link to={"/new"}>New</Link>
