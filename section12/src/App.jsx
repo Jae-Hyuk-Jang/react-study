f42d0e0e76e2dac947e0a89aa7486dcad20c6c07
Jae-Hyuk-Jang <lucentejjh@gmail.com>
2026-05-10 09:54:04 +0900

    add: pageRouter

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,7 +1,34 @@
 import "./App.css";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import Diary from "./pages/Diary";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Notfound from "./pages/Notfound";
 
+// 1. "/" : 모든 일기를 조회하는 Home 페이지
+// 2. "/new" : 새로운 일기를 작성하는 New 페이지
+// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
-  return <></>;
+  const nav = useNavigate();
+  const onClickButton = () => {
+    nav("/new");
+  };
+  return (
+    <>
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/new"}>New</Link>
+        <Link to={"/Diary"}>Diary</Link>
+      </div>
+      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary/:id" element={<Diary />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
